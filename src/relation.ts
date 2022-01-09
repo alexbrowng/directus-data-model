6a195f078350c33bd992664aa15ee3a471de3243
Alejandro Castaño González <alejandro@theseedsquad.com>
2022-01-09 18:43:51 +0100

    Update relations

--- src/relation.ts
+++ src/relation.ts
@@ -1,12 +1,22 @@
 import { Builder } from "./builder";
 
 export type onDelete = "NO ACTION" | "SET NULL" | "SET DEFAULT" | "CASCADE" | "RESTRICT";
+export type onUpdate = "NO ACTION";
+export type onDeselect = "nullify" | "delete";
 
 export type RelationSchema = {
   on_delete?: onDelete;
+  on_update?: onUpdate;
 };
 
-export type RelationMeta = {};
+export type RelationMeta = {
+  one_field?: null | string;
+  junction_field?: null | string;
+  sort_field?: null | string;
+  one_deselect_action?: onDeselect;
+  one_collection_field?: null | string;
+  one_allowed_collections?: null | string[];
+};
 
 export class Relation {
   builder: Builder;
@@ -32,8 +42,34 @@ export class Relation {
     this.meta = meta === undefined ? {} : meta;
   }
 
+  one_field(name: string) {
+    this.meta.one_field = name;
+    return this;
+  }
+
+  one_deselect_action(name: onDeselect) {
+    this.meta.one_deselect_action = name;
+    return this;
+  }
+
+  junction_field(name: string) {
+    this.meta.junction_field = name;
+    return this;
+  }
+
+  sort_field(name: string) {
+    this.meta.sort_field = name;
+    return this;
+  }
+
+  on_update(option: onUpdate) {
+    this.schema.on_update = option;
+    return this;
+  }
+
   on_delete(option: onDelete) {
     this.schema.on_delete = option;
+    return this;
   }
 
   render() {
